29c13c7a3e2952972feac49d36a5a5996f2b453c
Thibaud <thibaudroussel199995@gmail.com>
2021-12-20 14:48:39 +0100

    Commit spoùme file

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function NotFound() {
+    return (
+        <section>
+            <div>
+                <h1>4040 Not fou</h1>
+            </div>
+        </section>
+    )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Pa</h1>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem</p>
+            <p>Lorem </p>
+        </div>
+    )
+}
